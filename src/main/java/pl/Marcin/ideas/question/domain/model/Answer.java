315f1077b68bae8445dc6cba69f711a7a5a53d7c
Marcin <mkulczycki@o2.pl>
2022-11-10 18:52:55 +0100

    commit #2

--- src/main/java/pl/Marcin/ideas/question/domain/model/Answer.java
+++ src/main/java/pl/Marcin/ideas/question/domain/model/Answer.java
@@ -1,4 +1,15 @@
 package pl.Marcin.ideas.question.domain.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
 public class Answer {
+
+    private UUID id;
+
+    private String name;
 }
