52dc5740c25f2ae0fc901172514728ddbb40fe88
Marcin <mkulczycki@o2.pl>
2022-11-10 18:46:54 +0100

    Initial commit

---
+++ src/test/java/pl/Marcin/ideas/IdeasApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.Marcin.ideas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
