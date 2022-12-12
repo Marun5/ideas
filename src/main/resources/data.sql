74db85811a9f5cd122b5a5addf8e2270e45bb6a8
Marcin <mkulczycki@o2.pl>
2022-12-12 14:30:29 +0100

    update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,11 +15,14 @@ INSERT INTO categories (id, name) VALUES
 insert into questions (id, name, category_id) values
       (gen_random_uuid(), 'What to visit in Paris?', (select id from categories where name = 'Travel')),
       (gen_random_uuid(), 'What to visit in Italy?', (select id from categories where name = 'Travel')),
+      (gen_random_uuid(), 'What is your favourite type of food?', (select id from categories where name = 'Cooking')),
       (gen_random_uuid(), 'What are the rules of squash?', (select id from categories where name = 'Sport')),
       (gen_random_uuid(), 'What are the dimensions of the tennis cort?', (select id from categories where name = 'Sport')),
+      (gen_random_uuid(), 'Who is your favourite volleyball player?', (select id from categories where name = 'Sport')),
       (gen_random_uuid(), 'Who won football World Cup in Mexico in 1986?', (select id from categories where name = 'Sport')),
       (gen_random_uuid(), 'How much does a golf ball weigh?', (select id from categories where name = 'Sport')),
       (gen_random_uuid(), 'New movies in 2023', (select id from categories where name = 'Film')),
+      (gen_random_uuid(), 'Instruments you like to play on', (select id from categories where name = 'Music')),
       (gen_random_uuid(), 'What predatory animals do you know?', (select id from categories where name = 'Animals')),
       (gen_random_uuid(), 'What are the most healthy vitamins?', (select id from categories where name = 'Health')),
       (gen_random_uuid(), 'How often should you exercise?', (select id from categories where name = 'Health'));
@@ -27,6 +30,12 @@ insert into questions (id, name, category_id) values
 insert into answers (id, name, question_id) values
       (gen_random_uuid(), 'At least three times a week', (select id from questions where name = 'How often should you exercise?')),
       (gen_random_uuid(), 'Everyday is perfect', (select id from questions where name = 'How often should you exercise?')),
+      (gen_random_uuid(), 'Italian cuisine', (select id from questions where name = 'What is your favourite type of food?')),
+      (gen_random_uuid(), 'Pizza', (select id from questions where name = 'What is your favourite type of food?')),
+      (gen_random_uuid(), 'Fast food', (select id from questions where name = 'What is your favourite type of food?')),
+      (gen_random_uuid(), 'Drums', (select id from questions where name = 'Instruments you like to play on')),
+      (gen_random_uuid(), 'Guitar', (select id from questions where name = 'Instruments you like to play on')),
+      (gen_random_uuid(), 'Violin', (select id from questions where name = 'Instruments you like to play on')),
       (gen_random_uuid(), 'Cheetah', (select id from questions where name = 'What predatory animals do you know?')),
       (gen_random_uuid(), 'Lion', (select id from questions where name = 'What predatory animals do you know?')),
       (gen_random_uuid(), 'Tiger', (select id from questions where name = 'What predatory animals do you know?')),
@@ -47,4 +56,8 @@ insert into questions (id, name, category_id) values
 
 insert into answers (id, name, question_id) values
     (gen_random_uuid(), 'The Cat Who Caught a Killer', (select id from questions where name = 'Any good crime books?')),
-    (gen_random_uuid(), 'Dirty Town', (select id from questions where name = 'Any good crime books?'));
+    (gen_random_uuid(), 'Dirty Town', (select id from questions where name = 'Any good crime books?')),
+    (gen_random_uuid(), 'Wilfredo Leon', (select id from questions where name = 'Who is your favourite volleyball player?')),
+    (gen_random_uuid(), 'Kamil Semeniuk', (select id from questions where name = 'Who is your favourite volleyball player?')),
+    (gen_random_uuid(), 'Paweł Zatorski', (select id from questions where name = 'Who is your favourite volleyball player?')),
+    (gen_random_uuid(), 'Bartosz Kurek', (select id from questions where name = 'Who is your favourite volleyball player?'));
