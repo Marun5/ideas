3dfb69bcc4ada7ae13c78b9b8e93abab178a9c0c
Marcin <mkulczycki@o2.pl>
2022-12-12 08:46:27 +0100

    data.sql to english

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,4 @@
 FROM openjdk:19-jdk-alpine
-#ARG JAR_FILE=target/*.jar
-#COPY ${JAR_FILE} app.jar
 ADD target/ideas.jar ideas.jar
 ENTRYPOINT ["java", "-jar", "ideas.jar"]
 EXPOSE 8080
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,38 +13,38 @@ INSERT INTO categories (id, name) VALUES
           (gen_random_uuid(), 'Animals');
 
 insert into questions (id, name, category_id) values
-      (gen_random_uuid(), 'Co zwiedzić w Paryżu?', (select id from categories where name = 'Travel')),
-      (gen_random_uuid(), 'Co zwiedzić we Włoszech?', (select id from categories where name = 'Travel')),
-      (gen_random_uuid(), 'Jakie są zasady gry w squasha?', (select id from categories where name = 'Sport')),
-      (gen_random_uuid(), 'Jakie są wymiary boiska do gry w tenisa?', (select id from categories where name = 'Sport')),
-      (gen_random_uuid(), 'Kto wygrał mistrzostwa świata w piłce nożnej w Meksyku w 1986 roku?', (select id from categories where name = 'Sport')),
-      (gen_random_uuid(), 'Ile waży piłka do golfa?', (select id from categories where name = 'Sport')),
-      (gen_random_uuid(), 'Nowości 2023', (select id from categories where name = 'Film')),
-      (gen_random_uuid(), 'Jakie znasz drapieżne zwierzę?', (select id from categories where name = 'Animals')),
-      (gen_random_uuid(), 'Jakie są najzdrowsze witaminy?', (select id from categories where name = 'Health')),
-      (gen_random_uuid(), 'Jak często powinno się uprawiać sport?', (select id from categories where name = 'Health'));
+      (gen_random_uuid(), 'What to visit in Paris?', (select id from categories where name = 'Travel')),
+      (gen_random_uuid(), 'What to visit in Italy?', (select id from categories where name = 'Travel')),
+      (gen_random_uuid(), 'What are the rules of squash?', (select id from categories where name = 'Sport')),
+      (gen_random_uuid(), 'What are the dimensions of the tennis cort?', (select id from categories where name = 'Sport')),
+      (gen_random_uuid(), 'Who won football World Cup in Mexico in 1986?', (select id from categories where name = 'Sport')),
+      (gen_random_uuid(), 'How much does a golf ball weigh?', (select id from categories where name = 'Sport')),
+      (gen_random_uuid(), 'New movies in 2023', (select id from categories where name = 'Film')),
+      (gen_random_uuid(), 'What predatory animals do you know?', (select id from categories where name = 'Animals')),
+      (gen_random_uuid(), 'What are the most healthy vitamins?', (select id from categories where name = 'Health')),
+      (gen_random_uuid(), 'How often should you exercise?', (select id from categories where name = 'Health'));
 
 insert into answers (id, name, question_id) values
-      (gen_random_uuid(), 'Przynajmniej trzy razy w tygodniu', (select id from questions where name = 'Jak często powinno się uprawiać sport?')),
-      (gen_random_uuid(), 'Najlepiej codziennie', (select id from questions where name = 'Jak często powinno się uprawiać sport?')),
-      (gen_random_uuid(), 'Gepard', (select id from questions where name = 'Jakie znasz drapieżne zwierzę?')),
-      (gen_random_uuid(), 'Lew', (select id from questions where name = 'Jakie znasz drapieżne zwierzę?')),
-      (gen_random_uuid(), 'Tygrys', (select id from questions where name = 'Jakie znasz drapieżne zwierzę?')),
-      (gen_random_uuid(), 'Aligator', (select id from questions where name = 'Jakie znasz drapieżne zwierzę?')),
-      (gen_random_uuid(), 'Rekin', (select id from questions where name = 'Jakie znasz drapieżne zwierzę?'));
+      (gen_random_uuid(), 'At least three times a week', (select id from questions where name = 'How often should you exercise?')),
+      (gen_random_uuid(), 'Everyday is perfect', (select id from questions where name = 'How often should you exercise?')),
+      (gen_random_uuid(), 'Cheetah', (select id from questions where name = 'What predatory animals do you know?')),
+      (gen_random_uuid(), 'Lion', (select id from questions where name = 'What predatory animals do you know?')),
+      (gen_random_uuid(), 'Tiger', (select id from questions where name = 'What predatory animals do you know?')),
+      (gen_random_uuid(), 'Alligator', (select id from questions where name = 'What predatory animals do you know?')),
+      (gen_random_uuid(), 'Shark', (select id from questions where name = 'What predatory animals do you know?'));
 
 insert into answers (id, name, question_id) values
-    (gen_random_uuid(), 'Pola Elizejskie', (select id from questions where name = 'Co zwiedzić w Paryżu?')),
-    (gen_random_uuid(), 'Wieża Eiffla', (select id from questions where name = 'Co zwiedzić w Paryżu?')),
-    (gen_random_uuid(), 'Piza', (select id from questions where name = 'Co zwiedzić we Włoszech?')),
-    (gen_random_uuid(), 'Neapol', (select id from questions where name = 'Co zwiedzić we Włoszech?')),
-    (gen_random_uuid(), 'Rzym', (select id from questions where name = 'Co zwiedzić we Włoszech?')),
-    (gen_random_uuid(), 'Wenecja', (select id from questions where name = 'Co zwiedzić we Włoszech?'));
+    (gen_random_uuid(), 'Avenue des Champs-Elysees', (select id from questions where name = 'What to visit in Paris?')),
+    (gen_random_uuid(), 'Eiffel Tower', (select id from questions where name = 'What to visit in Paris?')),
+    (gen_random_uuid(), 'Piza', (select id from questions where name = 'What to visit in Italy?')),
+    (gen_random_uuid(), 'Napoli', (select id from questions where name = 'What to visit in Italy?')),
+    (gen_random_uuid(), 'Rome', (select id from questions where name = 'What to visit in Italy?')),
+    (gen_random_uuid(), 'Venice', (select id from questions where name = 'What to visit in Italy?'));
 
 insert into questions (id, name, category_id) values
-      (gen_random_uuid(), 'Jakie znacie fajne kryminały?', (select id from categories where name = 'Books')),
-      (gen_random_uuid(), 'Jakie polecanie książki romantyczne?', (select id from categories where name = 'Books'));
+      (gen_random_uuid(), 'Any good crime books?', (select id from categories where name = 'Books')),
+      (gen_random_uuid(), 'Which romance books do you recommend?', (select id from categories where name = 'Books'));
 
 insert into answers (id, name, question_id) values
-    (gen_random_uuid(), 'Kryminał 1', (select id from questions where name = 'Jakie znacie fajne kryminały?')),
-    (gen_random_uuid(), 'Kryminał 2', (select id from questions where name = 'Jakie znacie fajne kryminały?'));
+    (gen_random_uuid(), 'The Cat Who Caught a Killer', (select id from questions where name = 'Any good crime books?')),
+    (gen_random_uuid(), 'Dirty Town', (select id from questions where name = 'Any good crime books?'));
